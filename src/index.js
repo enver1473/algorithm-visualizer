5ace68d4de7ab68ec94857acb0d0290bba80179f
enver1473 <enverkovacevic@hotmail.com>
2021-08-01 19:58:06 +0200

    Minor reorganize

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import 'p5/lib/addons/p5.sound';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "p5/lib/addons/p5.sound";
+import "./index.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 serviceWorker.unregister();
